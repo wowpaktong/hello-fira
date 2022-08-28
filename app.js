d0cac0e54f58e372fa6813796f442051d0346571
wowpaktong <wowpaktong@gmail.com>
2022-08-27 21:48:44 -0700

    tanggal

--- app.js
+++ app.js
@@ -9,17 +9,22 @@ const countdown = () => {
   const hours = minutes * 60
   const days = hours * 24
 
-  let timeDays = Math.floor(difference / days)
+  let timeDays =  difference < 0 ? "00" : Math.floor(difference / days)
   let timeHours = Math.floor((difference % days) / hours)
   let timeMinutes = Math.floor((difference % hours) / minutes)
   let timeSeconds = Math.floor((difference % minutes) / seconds)
-  timeHours = timeHours < 10 ? "0" + timeHours : timeHours
-  timeMinutes = timeMinutes < 10 ? "0" + timeHours :timeMinutes
-  timeSeconds = timeMinutes < 10 ? "0" + timeMinutes : timeSeconds
+  timeHours = difference < 0 ? "00" : timeHours < 10 ? "0" + timeHours : timeHours
+  timeMinutes =  difference < 0 ? "00" : timeMinutes < 10 ? "0" + timeHours :timeMinutes
+  timeSeconds =  difference < 0 ? "00" : timeMinutes < 10 ? "0" + timeMinutes : timeSeconds
 
   document.getElementById("Days").innerHTML = timeDays
   document.getElementById("Hours").innerHTML = timeHours
   document.getElementById("Minutes").innerHTML = timeMinutes
   document.getElementById("Seconds").innerHTML = timeSeconds
+
+  if (difference < 0 ) {
+    clearInterval();
+  }
 }
+
 setInterval(countdown, 1000)
